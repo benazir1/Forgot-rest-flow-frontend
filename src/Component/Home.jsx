68090b2cf53cf0ead7f6035e6689611f2500e36a
benazir1 <benazir1989@gmail.com>
2023-09-27 13:40:47 +0530

    first commit

---
+++ src/Component/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import {useNavigate } from 'react-router-dom'
+import "../App.css";
+function Home() {
+  const navigate = useNavigate();
+  const email = localStorage.getItem('EMAIL')
+  useEffect(()=>{
+    const token = localStorage.getItem('TOKEN');
+    if(!token){
+      navigate('/signup');
+    }
+
+  }, [])
+  return (
+    <div className='card'>
+    <div>Home</div>
+    <div>
+    
+       <button onClick={()=>{
+        localStorage.clear();
+        navigate('/signIN');
+       }}>Logout</button>
+    </div>
+    </div>
+  )
+}
+
+export default Home
